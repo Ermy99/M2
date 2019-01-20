0c48e7ce3f0248c0b849a7424a323552c7825e1e
Aditya Tapshalkar <aditya.taps@gatech.edu>
2019-01-20 02:38:41 -0500

    committing person1 method
    A

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Aditya Tapshalkar
  *  @version 1.1
  */
 public class Person1 {
@@ -29,9 +29,18 @@ public class Person1 {
 	 * @param input the string to be modified
 	 * @return the modified string
 	 */
-	private String calc(String input) {
+	private static String calc(String input) {
+
 	  //Person 1 put your implementation here
-	  return null;
+
+        String toReturn = "";
+
+		if (input.length() <= 2) {
+			return input;
+		}
+
+		return input.substring(2) + input.substring(0,2);
+
 	}
 	
 	/**
@@ -40,10 +49,12 @@ public class Person1 {
 	 *
 	 * @param input the varying string
 	 * @return the string representing the 
-	 *         object
+	 * object
 	 */
 	public String toString(String input) {
+
 	  return name + calc(input);
+
 	}
 
 }

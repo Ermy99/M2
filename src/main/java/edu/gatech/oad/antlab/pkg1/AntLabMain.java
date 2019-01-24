c835e9585071cfaae0723a6ae1b6d36fdf8e2cb2
somiobed <somi.obed@gmail.com>
2019-01-24 16:10:32 -0500

    updated

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -72,8 +72,8 @@ import edu.gatech.oad.antlab.pkg2.*;
 
 		  //Person4 replace P4 with your name
           //and gburdell4 with your gt id
-          Person4 p4 = new Person4("P4");
-          toPrint += p4.toString("gburdell4");
+          Person4 p4 = new Person4("Chisomebi Obed");
+          toPrint += p4.toString("cobed3");
 
           //Person5 replace P4 with your name
           //and gburdell5 with your gt id
